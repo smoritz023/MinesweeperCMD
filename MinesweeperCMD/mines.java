6a561dbaa243b9b42d60188990ca269a0839032a
Sam Moritz <91634909+smoritz023@users.noreply.github.com>
2022-02-21 16:21:17 -0600

    recursion added, not complete

--- MinesweeperCMD/mines.java
+++ MinesweeperCMD/mines.java
@@ -20,6 +20,7 @@ public class mines {
         int d = 15;
         
         int[][] playField = initializeBoard(x, y);
+        int[][] showBoard = initializeBoard(x, y);
 
         // printBoard(playField, x, y);
 
@@ -28,27 +29,50 @@ public class mines {
         // printBoard(playField, x, y);
 
         placeNumbers(playField, x, y);
-        printBoard(playField, x, y);
+        // printBoard(playField, showBoard, x, y);
+
+        digHole(playField, showBoard, 4, 6);
+        // printBoard(playField, showBoard, x, y);
+
+        System.out.println("\nprint showBoard");
+        printShowBoard(playField, showBoard, x, y);
+        printMineBoard(playField, showBoard, x, y);
     }
 
-    public static void printBoard(int[][] board, int x, int y){
-        System.out.println("");
-        System.out.println("Print Board Method");
+    public static void printShowBoard(int[][] board, int[][] showBoard, int x, int y){
         for(int i = 0; i < x; i++){
             for(int j = 0; j < y; j++){
                 // board[0][1] = 99;
-                if(board[i][j] == 9){
-                    System.out.print("F" + " ");
+                if(showBoard[i][j] == 1){
+                    if(board[i][j] == 9){
+                        System.out.print("F" + " ");
+                    }
+                    else if(board[i][j] == 10){
+                        System.out.print("M" + " ");
+                    }
+                    else if(board[i][j] == 0){
+                        System.out.print("0" + " ");
+                    }
+                    else if((board[i][j] > 0) && (board[i][j] < 9)){
+                        System.out.print(board[i][j] + " ");
+                    }
+                    else{
+                        System.out.print(board[i][j] + " ");
+                    }
                 }
-                else if(board[i][j] == 10){
-                    System.out.print("M" + " ");
+                else if(showBoard[i][j] == 0){
+                    System.out.print(" " + " ");
                 }
-                else if(board[i][j] == 0){
-                    System.out.print("0" + " ");
-                }
-                else{
-                    System.out.print(board[i][j] + " ");
-                }
+
+            }
+            System.out.println();
+        }
+    }
+
+    public static void printMineBoard(int[][] board, int[][] showBoard, int x, int y){
+        for(int i = 0; i < x; i++){
+            for(int j = 0; j < y; j++){
+                System.out.print(board[i][j] + " ");
             }
             System.out.println();
         }
@@ -59,6 +83,14 @@ public class mines {
         return board;
     }
 
+    public static int[][] setShowBoard(int[][] showBoard, int x, int y){
+        for(int i = 0; i < x; i++){
+            for(int j = 0; j < y; i++){
+                showBoard[i][j] = 0;
+            }
+        }
+        return showBoard;
+    }
 
     public static int[][] placeMines(int[][] board, int x, int y, int d){
         Random rand = new Random();
@@ -67,9 +99,10 @@ public class mines {
             int randY = rand.nextInt(y);
             if(board[randY][randX] != 10){
                 board[randY][randX] = 10;
-                System.out.println("Mines remaining " + d);
+                // System.out.println("Mines remaining " + d);
                 d--;
             }
+            // board[3][5] = 10;
         }
         return board;
     }
@@ -242,6 +275,36 @@ public class mines {
 
                 // }
             }
+        }
+        return board;
+    }
+
+    public static int[][] digHole(int[][] board, int[][] showBoard, int x, int y){
+        int adjustedX = x - 1;
+        int adjustedY = y - 1;
+        // board[adjustedX][adjustedY] = 90;
+        if((adjustedX >= 0) && (adjustedX <= x) && (adjustedY >= 0) && (adjustedX <= y) && (showBoard[adjustedX][adjustedY] == 0)){
+            if(board[adjustedX][adjustedY] == 10){
+                System.out.println("hit a bomb at " + adjustedX + ":" + adjustedY);
+            }
+            else if((board[adjustedX][adjustedY] > 0) && (board[adjustedX][adjustedY] < 9)){
+                System.out.println("hit a number at " + adjustedX + ":" + adjustedY);
+                return board;
+            }
+            else{
+                if((adjustedX > 0) && (adjustedX < x) && (adjustedY > 0) && (adjustedY < y)){
+                    System.out.println("hit a zero at " + adjustedX + ":" + adjustedY);
+                    digHole(board, showBoard, adjustedX-1, adjustedY-1);
+                    digHole(board, showBoard, adjustedX, adjustedY-1);
+                    digHole(board, showBoard, adjustedX+1, adjustedY-1);
+                    digHole(board, showBoard, adjustedX+1, adjustedY);
+                    // digHole(board, adjustedX+1, adjustedY+1);
+                    digHole(board, showBoard, adjustedX, adjustedY+1);
+                    digHole(board, showBoard, adjustedX-1, adjustedY+1);
+                    digHole(board, showBoard, adjustedX-1, adjustedY);
+                }
+            }
+            showBoard[adjustedX][adjustedY] = 1;
         }
         return board;
     }
